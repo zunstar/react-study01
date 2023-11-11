7542325f50a790dbbf98535f62e9f9688eb962cb
zunstar <shimkijun@gmail.com>
2023-11-11 23:12:22 +0900

    useMemo 를 사용하여 연산한 값 재사용하기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import React, { useRef ,useState } from 'react';
+import React, { useRef ,useState, useMemo } from 'react';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
+
+function countActiveUsers(users){
+  console.log('활성 사용자 수를 세는중...');
+  return users.filter(user => user.active).length;
+}
 
 function App() {
 
@@ -55,6 +60,17 @@ function App() {
     );
   }
 
+  // useMemo 를 사용하여 연산한 값 재사용하기
+  // useMemo 의 첫번째 파라미터에는 어떻게 연산할지 정의하는 함수를 넣어주면 되고,
+  // 두번째 파라미터에는 deps 배열을 넣어주면 되는데, 이 배열 안에 넣은 내용이 바뀌면,
+  // 우리가 등록한 함수를 호출해서 값을 연산해주고, 만약에 내용이 바뀌지 않았다면 이전에 연산한 값을 재사용하게 됩니다.
+  // 이렇게 함으로써, 렌더링하는 과정에서 함수형 컴포넌트 내부에서 발생하는 연산을 최적화 할 수 있습니다.
+  // 이번에는 users 배열을 파라미터로 넣어주었는데, 이 배열 내부의 값이 바뀔 때에만 함수를 호출하고,
+  // 만약에 배열 내부의 값이 바뀌지 않았다면 이전에 연산한 값을 재사용하게 됩니다.
+  // 이렇게 함으로써, users 배열 내부의 값이 바뀌지 않았다면, 즉 onCreate, onRemove, onToggle 함수가 호출되었을 때만
+  // countActiveUsers 함수가 호출되고, 그렇지 않다면 호출되지 않습니다.
+  const count = useMemo(() => countActiveUsers(users),[users]);
+
   return (
     <>
       <CreateUser
@@ -64,6 +80,7 @@ function App() {
         onCreate={onCreate}
       />
       <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+      <div>활성 사용자 수 : {count}</div>
     </>
   );
 }
--- src/UserList.js
+++ src/UserList.js
@@ -3,28 +3,8 @@ import React,{useEffect} from 'react';
 function User({user,onRemove,onToggle}){
     const {username,email,id,active} = user;
 
-    // // [] 빈 배열을 넣으면 컴포넌트가 처음 나타날 때만 useEffect 에 등록한 함수가 호출됨
-    // useEffect(()=>{ 
-    //     console.log('컴포넌트가 화면에 나타남');
-    //     // props -> state
-    //     // REST API
-    //     // D3 Video.js
-    //     // setInterval, setTimeout
-    //     return () => {
-    //         // clearInterval, clearTimeout
-    //         // 라이브러리 인스턴스 제거
-    //         console.log('컴포넌트가 화면에서 사라짐');
-    //     }
-    // },[]);
-
-    // // [값] 배열에는 useEffect 의 함수가 실행될 때 특정 값이 업데이트 될 때만 호출하고 싶을 때 사용
     useEffect(()=>{ 
-        console.log('user 값이 설정됨');
-        console.log(user);
-        return () => {
-            console.log('user 값이 바뀌기 전');
-            console.log(user);
-        }
+       
     },[user]);
     
     return(
