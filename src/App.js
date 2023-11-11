66b8aa3c9c66e05fe7248e01e5b2c51c8e679fa0
zunstar <shimkijun@gmail.com>
2023-11-11 17:50:34 +0900

    JSX

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import Hello from './Hello';
+import './App.css';
+
 function App() {
+  const name = 'react'
+  const style = {
+    backgroundColor:'black',
+    color:'aqua',
+    fontSize:24,
+    padding:'1rem'
+  }
   return (
-    <div>
-      <Hello />
-    </div>
+      <>
+        {/* 주석 */}
+        <Hello 
+          //  태그 주석
+        />
+        <div
+          // 태그 주석2
+        className='gray-box' style={style}>{name}</div>
+      </>
   );
 }
 
